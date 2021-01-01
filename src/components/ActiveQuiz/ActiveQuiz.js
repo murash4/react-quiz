8b0d88de44d98f94dbceebca3c6a9698072818d3
murash4 <singleant@gmail.com>
2021-01-01 16:50:57 +0300

    add ActiveQuiz component

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,1 +1,24 @@
 import classes from './ActiveQuiz.module.scss'
+
+
+
+const ActiveQuiz = props => (
+	<div className={ classes.ActiveQuiz }>
+		<p className={ classes['ActiveQuiz__question'] }>
+			<span>
+				<strong>2. </strong>
+				Как дела?
+			</span>
+			<small>4 из 12</small>
+		</p>
+
+		<ul>
+			<li>1</li>
+			<li>2</li>
+			<li>3</li>
+			<li>4</li>
+		</ul>
+	</div>
+)
+
+export default ActiveQuiz
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import classes from './Quiz.module.scss'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
 
 class Quiz extends Component {
 	state = {
@@ -9,7 +10,10 @@ class Quiz extends Component {
 	render () {
 		return (
 			<div className={ classes.Quiz }>
-				<h1 className={ classes['Quiz__h1'] }>Quiz</h1>
+				<div className={ classes['Quiz__wrapper'] }>
+					<h1 className={ classes['Quiz__h1'] }>Quiz</h1>
+					<ActiveQuiz />
+				</div>
 			</div>
 		)
 	}
