c6cc7048fe346ca60b7f637536b676519aa8f41e
murash4 <singleant@gmail.com>
2021-01-01 21:57:31 +0300

    menu toggle button

---
+++ src/components/Navigation/MenuToggle/MenuToggle.js
@@ -0,0 +1,17 @@
+import './MenuToggle.scss'
+
+const MenuToggle = props => {
+	const cls = [
+		'MenuToggle fa',
+		props.isOpen ? 'fa-times open' : 'fa-bars'
+	]
+
+	return (
+		<i
+			className={ cls.join(' ') }
+			onClick={ props.onToggle }
+		/>
+	)
+}
+
+export default MenuToggle
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,26 @@
 import { Component } from 'react'
 import classes from './Layout.module.scss'
+import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle'
 
 class Layout extends Component {
+	state = {
+		menu: false
+	}
+
+	toggleMenuHandler = () => {
+		this.setState({
+				menu: !this.state.menu
+		})
+	}
+
 	render () {
 		return (
 			<div className={ classes.Layout }>
+				<MenuToggle
+					isOpen={ this.state.menu }
+					onToggle={ this.toggleMenuHandler }
+				/>
+
 				<main className={ classes['Layout-main'] }>
 					{ this.props.children }
 				</main>
