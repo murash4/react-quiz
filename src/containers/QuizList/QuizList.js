f986291ac2543c9042f219270a732d658a155f18
murash4 <singleant@gmail.com>
2021-01-03 00:26:31 +0300

    get tests from server

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -4,22 +4,45 @@ import { NavLink } from 'react-router-dom'
 import axios from 'axios'
 
 export default class QuizList extends Component {
+	state = {
+		quizes: []
+	}
 	renderQuizes () {
-		return [1, 2, 3].map((quiz, index) => {
+		return this.state.quizes.map(quiz => {
 			return (
 				<li
-					key={ index }
+					key={ quiz.id }
 					className="QuizList__li"
 				>
 					<NavLink
-						to={ '/quiz/' + quiz }
+						to={ '/quiz/' + quiz.id }
 						className="QuizList__link"
 					>
-						Тест { quiz }
+						{ quiz.name }
 					</NavLink>
 				</li>
 			)
 		})
+	}
+
+	async componentDidMount() {
+		try {
+			const response = await axios.get('https://react-quiz-93710-default-rtdb.firebaseio.com/quizes.json')
+			const quizes = []
+
+			Object.keys(response.data).forEach((key, index) => {
+				quizes.push({
+					id: key,
+					name: `Тест №${index + 1}`
+				})
+			})
+
+			this.setState({
+				quizes
+			})
+		} catch (error) {
+			console.log(error)
+		}
 	}
 
 	render () {
