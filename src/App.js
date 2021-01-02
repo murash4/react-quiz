3c21b5e1447d6e4ebc3e35e8343245615b4ee09a
murash4 <singleant@gmail.com>
2021-01-02 14:32:58 +0300

    add pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import Layout from './hoc/Layout/Layout'
 import Quiz from "./containers/Quiz/Quiz"
+import { Route, Switch } from 'react-router-dom'
+import QuizList from './containers/QuizList/QuizList'
+import Auth from './containers/Auth/Auth'
+import QuizCreator from './containers/QuizCreator/QuizCreator'
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Switch>
+        <Route
+          path="/"
+          component={ QuizList }
+        />
+
+        <Route
+          path="/auth"
+          component={ Auth }
+        />
+
+        <Route
+          path="/quiz-creator"
+          component={ QuizCreator }
+        />
+
+        <Route
+          path="/quiz/:id"
+          component={ Quiz }
+        />
+      </Switch>
     </Layout>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+
+const application = (
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  application,
   document.getElementById('root')
 );
 
