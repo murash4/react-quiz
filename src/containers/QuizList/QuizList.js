13ffc04a3a30c83755d04b726c9b308ad385dad8
murash4 <singleant@gmail.com>
2021-01-02 14:59:41 +0300

    modify quiz list page

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,10 +1,36 @@
 import { Component } from 'react'
+import "./QuizList.scss"
+import { NavLink } from 'react-router-dom'
 
 export default class QuizList extends Component {
+	renderQuizes () {
+		return [1, 2, 3].map((quiz, index) => {
+			return (
+				<li
+					key={ index }
+					className="QuizList__li"
+				>
+					<NavLink
+						to={ '/quiz/' + quiz }
+						className="QuizList__link"
+					>
+						Тест { quiz }
+					</NavLink>
+				</li>
+			)
+		})
+	}
+
 	render () {
 		return (
-			<div>
-				<h1>Quiz List</h1>
+			<div className="QuizList">
+				<div>
+					<h1 className="QuizList__h1">Список тестов</h1>
+
+					<ul className="QuizList__ul">
+						{ this.renderQuizes() }
+					</ul>
+				</div>
 			</div>
 		)
 	}
