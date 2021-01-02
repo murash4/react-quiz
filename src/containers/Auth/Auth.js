8dcd1449a93e3296f5dbc33e3b18446762211a3f
murash4 <singleant@gmail.com>
2021-01-02 15:53:18 +0300

    modify Auth page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,37 @@
 import { Component } from 'react'
+import './Auth.scss'
+import Button from '../../components/UI/Button/Button'
 
 export default class Auth extends Component {
+	loginHandler = () => {}
+
+	registerHandler = () => {}
+
+	submitHandler = event => {
+		event.preventDefault()
+	}
+
 	render () {
 		return (
-			<div>
-				<h1>Auth</h1>
+			<div className="Auth">
+				<div className="Auth__inner">
+					<h1 className="Auth__h1">Авторизация</h1>
+					<form
+						className="Auth__form"
+						onSubmit={ this.submitHandler }
+					>
+						<input type="text"/>
+						<input type="text"/>
+						<Button
+							type="success"
+							onClick={ () => this.loginHandler }
+						>Войти</Button>
+						<Button
+							type="primary"
+							onClick={ () => this.registerHandler }
+						>Зарегистрироваться</Button>
+					</form>
+				</div>
 			</div>
 		)
 	}
