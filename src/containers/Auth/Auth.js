e349af273bb611278050278c251d9d39d60589c4
murash4 <singleant@gmail.com>
2021-01-03 18:09:56 +0300

    redux setup

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import './Auth.scss'
 import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 import is from 'is_js'
+import axios from 'axios'
 
 export default class Auth extends Component {
 	state = {
@@ -35,9 +36,37 @@ export default class Auth extends Component {
 		}
 	}
 
-	loginHandler = () => {}
+	loginHandler = async () => {
+		const authData = {
+			email: this.state.formControls.email.value,
+			password: this.state.formControls.password.value,
+			returnSecureToken: true
+		}
 
-	registerHandler = () => {}
+		try {
+			const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA0RrDYyOW4n9aC5L-k5tk2W7ih8QHbMWQ`, authData)
+
+			console.log(response.data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	registerHandler = async () => {
+		const authData = {
+			email: this.state.formControls.email.value,
+			password: this.state.formControls.password.value,
+			returnSecureToken: true
+		}
+
+		try {
+			const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA0RrDYyOW4n9aC5L-k5tk2W7ih8QHbMWQ`, authData)
+
+			console.log(response.data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
 	submitHandler = event => {
 		event.preventDefault()
@@ -120,12 +149,12 @@ export default class Auth extends Component {
 						<Button
 							type="success"
 							disabled={ !this.state.isFormValid }
-							onClick={ () => this.loginHandler }
+							onClick={ () => this.loginHandler() }
 						>Войти</Button>
 						<Button
 							type="primary"
 							disabled={ !this.state.isFormValid }
-							onClick={ () => this.registerHandler }
+							onClick={ () => this.registerHandler() }
 						>Зарегистрироваться</Button>
 					</form>
 				</div>
